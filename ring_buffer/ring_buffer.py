1d3e665d3066cddd8de2bb2a9611f76b9e8fd571
RachelKolk <rach.m.obrien@gmail.com>
2019-05-17 11:43:57 -0400

    Working on ring-buffer

--- ring_buffer/ring_buffer.py
+++ ring_buffer/ring_buffer.py
@@ -5,7 +5,16 @@ class RingBuffer:
     self.storage = [None]*capacity
 
   def append(self, item):
-    pass
+    # checks to see if the buffer is at its max capacity
+    if len(self.storage) == self.capacity:
+      self.storage.pop(0)
+      
+    # appends the item at the end of the buffer
+    self.storage.append(item)
+    print("Appending to buffer. Buffer now: ", self.storage)
+
 
   def get(self):
-    pass
+    print("Capacity is: ", self.capacity)
+    print("Last index is: ", len(self.storage) - 1)
+    return self.storage
